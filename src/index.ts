d6101d963ee2191e2d88004964c4ccbe839a6989
kkpagaev <kkpagaev@gmail.com>
2023-02-23 13:57:36 +0200

    cors

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,15 @@ async function main() {
       extended: true,
     })
   )
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const cors = require("cors")
+  const corsOptions = {
+    origin: "*",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  }
+
+  app.use(cors(corsOptions)) //
 
   app.use(express.static("public"))
 
